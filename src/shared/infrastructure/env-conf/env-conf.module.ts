0d8895e6d1bf9e70b250c8eeee76a84ef135644d
Mateus Freitas Cavalcanti <mat.fcavalcanti@gmail.com>
2023-09-26 13:53:54 -0300

    Refactor env config module to custom .env files

--- src/shared/infrastructure/env-conf/env-conf.module.ts
+++ src/shared/infrastructure/env-conf/env-conf.module.ts
@@ -1,7 +1,18 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfService } from './env-conf.service';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'node:path';
 
 @Module({
   providers: [EnvConfService]
 })
-export class EnvConfModule {}
+
+export class EnvConfModule extends ConfigModule {
+  // Loads process environment variables
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return super.forRoot({
+      ...options,
+      envFilePath: [join(__dirname, `../../../../.env.${process.env.NODE_ENV}`)]
+    });
+  }
+}

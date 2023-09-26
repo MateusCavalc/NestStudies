2a9274b75babda8002cdc26e24a8383f8910c60b
Mateus Freitas Cavalcanti <mat.fcavalcanti@gmail.com>
2023-09-26 15:38:22 -0300

    Add 'users' resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfModule } from './shared/infrastructure/env-conf/env-conf.module';
+import { UsersModule } from './users/infrastructure/users.module';
 
 @Module({
-  imports: [EnvConfModule],
+  imports: [EnvConfModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

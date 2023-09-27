d905138cb1790be2d9523f3e477ba5e7819301ad
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2023-09-27 00:57:16 -0300

    create entity setters and public setter methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -17,12 +17,36 @@ export class UserEntity extends Entity {
         return this.props.name;
     }
 
+    private set name(value: string) {
+        this.props.name = value;
+    }
+
+    public setName(value: string) {
+        this.name = value;
+    }
+
     get email() {
         return this.props.email;
     }
 
+    private set email(value: string) {
+        this.props.email = value;
+    }
+
+    public setEmail(value: string) {
+        this.email = value;
+    }
+
     get password() {
         return this.props.password;
+    }
+
+    private set password(value: string) {
+        this.props.password = value;
+    }
+
+    public setPassword(value: string) {
+        this.password = value;
     }
 
     get createdAt() {

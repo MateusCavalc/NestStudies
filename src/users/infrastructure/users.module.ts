db60eb5a97a41c3c86a604ac7fa941c0084676f4
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2023-10-05 10:41:58 -0300

    Register SignUp UseCase in User Module

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SignUpUseCase } from '../application/usecases/signup.usecase';
+import { UserInMemoryRepository } from './database/in-memory/repositories/user.inMemory.repository';
+import { BcryptHashProvider } from './providers/bcrypt-hash.provider';
+import { UserRepository } from '../domain/repositories/user.repository';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptHashProvider
+    },
+    {
+      provide: SignUpUseCase.UseCase,
+      inject: ['UserRepository', 'HashProvider'],
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider
+      ) => { return new SignUpUseCase.UseCase(userRepository, hashProvider) }
+    }
+  ],
 })
-export class UsersModule {}
+export class UsersModule { }

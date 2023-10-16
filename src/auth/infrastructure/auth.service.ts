f12eb90465d20702c4b5e022fa22ff5465afbe47
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2023-10-16 12:06:29 -0300

    Implement Auth Service

--- src/auth/infrastructure/auth.service.ts
+++ src/auth/infrastructure/auth.service.ts
@@ -1,4 +1,23 @@
+import { EnvConfService } from '@/shared/infrastructure/env-conf/env-conf.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private jwtService: JwtService,
+        private configService: EnvConfService
+    ) { }
+
+    async generateJwt(userId: string): Promise<string> {
+        const token = await this.jwtService.signAsync(userId, {});
+        return token;
+    }
+
+    async verifyJwt(token: string) {
+        return this.jwtService.verifyAsync(token, {
+            secret: this.configService.getJwtSecret()
+        });
+    }
+
+}
